e1afef5f50f153c51eb92656547ef92839571a3f
Mikhei1i <135987132+Mikhei1i@users.noreply.github.com>
2023-07-17 23:00:35 +0400

    section 3 v1

---
+++ scripts/index.js
@@ -0,0 +1,24 @@
+function triggerAnimation() {
+    var box = document.getElementById("year1");
+    var text = document.getElementById("text1");
+    box.style.animation = 'slideIn 1s linear 1'
+    text.style.animation = 'slideIn2 1s linear 1'
+    text.style.animationDelay = '0.5s'
+    text.style.opacity = '1'
+  }
+
+  function handleScroll() {
+    var box = document.getElementById("s3");
+    var boxOffsetTop = box.offsetTop;
+    var windowHeight = window.innerHeight;
+    var scrollY = window.scrollY;
+
+    var triggerOffset = 100;
+
+    if (scrollY > boxOffsetTop - windowHeight + triggerOffset) {
+      triggerAnimation();
+      window.removeEventListener("scroll", handleScroll);
+    }
+  }
+
+  window.addEventListener("scroll", handleScroll);
