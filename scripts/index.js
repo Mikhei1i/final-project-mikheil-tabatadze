ac0c9efbba15434abb3409b51262aac1265f6d7b
Mikhei1i <135987132+Mikhei1i@users.noreply.github.com>
2023-07-18 21:48:30 +0400

    section 3 final

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,10 @@
 function triggerAnimation() {
-    var box = document.getElementById("year1");
-    var text = document.getElementById("text1");
-    box.style.animation = 'slideIn 1s linear 1'
-    text.style.animation = 'slideIn2 1s linear 1'
-    text.style.animationDelay = '0.2s'
-    text.style.opacity = '1'
+    var box = document.getElementById("winner");
+    box.style.opacity = '1'
   }
 
   function handleScroll() {
-    var box = document.getElementById("s3");
+    var box = document.getElementById("winner");
     var boxOffsetTop = box.offsetTop;
     var windowHeight = window.innerHeight;
     var scrollY = window.scrollY;
