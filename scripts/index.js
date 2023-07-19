098f3fa94cb33ddfe7b2befb135a45ec424e7d64
Mikhei1i <135987132+Mikhei1i@users.noreply.github.com>
2023-07-19 16:12:13 +0400

    section 5

--- scripts/index.js
+++ scripts/index.js
@@ -48,3 +48,29 @@ function winner2 () {
 }
 
 winner2()
+
+// winner 3
+
+function winner3 () {
+  function triggerAnimation() {
+    var box = document.getElementById("winner3");
+    box.style.opacity = '1'
+  }
+  
+  function handleScroll() {
+    var box = document.getElementById("winner3");
+    var boxOffsetTop = box.offsetTop;
+    var windowHeight = window.innerHeight;
+    var scrollY = window.scrollY;
+    var triggerOffset = 100;
+  
+    if (scrollY > boxOffsetTop - windowHeight + triggerOffset) {
+      triggerAnimation();
+      window.removeEventListener("scroll", handleScroll);
+    }
+  }
+  
+  window.addEventListener("scroll", handleScroll);
+}
+
+winner3()
