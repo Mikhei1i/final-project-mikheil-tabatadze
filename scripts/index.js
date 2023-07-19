8fb9e43c4489f95856a9be085808c663725e15f1
Mikhei1i <135987132+Mikhei1i@users.noreply.github.com>
2023-07-19 22:20:40 +0400

    section 8

--- scripts/index.js
+++ scripts/index.js
@@ -127,6 +127,33 @@ function winner5 () {
 
 winner5()
 
+// winner 6 
+// winner 5
+
+function winner6 () {
+  function triggerAnimation() {
+    var box = document.getElementById("winner6");
+    box.style.opacity = '1'
+  }
+  
+  function handleScroll() {
+    var box = document.getElementById("winner6");
+    var boxOffsetTop = box.offsetTop;
+    var windowHeight = window.innerHeight;
+    var scrollY = window.scrollY;
+    var triggerOffset = 100;
+  
+    if (scrollY > boxOffsetTop - windowHeight + triggerOffset) {
+      triggerAnimation();
+      window.removeEventListener("scroll", handleScroll);
+    }
+  }
+  
+  window.addEventListener("scroll", handleScroll);
+}
+
+winner6()
+
 function playVid(card) {
   const video = card.querySelector('.vid1');
   video.setAttribute('autoplay', 'true');
