ae895db70123c443d4d2ebb59b90e89cae5cbc2e
Mikhei1i <135987132+Mikhei1i@users.noreply.github.com>
2023-07-20 20:16:14 +0400

    y2022 & hamburger menu v1

--- scripts/index.js
+++ scripts/index.js
@@ -1,184 +1,3 @@
-// winner 1
-function winner() {
-  function triggerAnimation() {
-    var box = document.getElementById("winner");
-    box.style.opacity = '1'
-  }
-
-  function handleScroll() {
-    var box = document.getElementById("winner");
-    var boxOffsetTop = box.offsetTop;
-    var windowHeight = window.innerHeight;
-    var scrollY = window.scrollY;
-    var triggerOffset = 100;
-
-    if (scrollY > boxOffsetTop - windowHeight + triggerOffset) {
-      triggerAnimation();
-      window.removeEventListener("scroll", handleScroll);
-    }
-  }
-
-  window.addEventListener("scroll", handleScroll);
-}
-
-winner()
-
-
-// winner 2
-function winner2 () {
-  function triggerAnimation() {
-    var box = document.getElementById("winner2");
-    box.style.opacity = '1'
-  }
-  
-  function handleScroll() {
-    var box = document.getElementById("winner2");
-    var boxOffsetTop = box.offsetTop;
-    var windowHeight = window.innerHeight;
-    var scrollY = window.scrollY;
-    var triggerOffset = 100;
-  
-    if (scrollY > boxOffsetTop - windowHeight + triggerOffset) {
-      triggerAnimation();
-      window.removeEventListener("scroll", handleScroll);
-    }
-  }
-  
-  window.addEventListener("scroll", handleScroll);
-}
-
-winner2()
-
-// winner 3
-
-function winner3 () {
-  function triggerAnimation() {
-    var box = document.getElementById("winner3");
-    box.style.opacity = '1'
-  }
-  
-  function handleScroll() {
-    var box = document.getElementById("winner3");
-    var boxOffsetTop = box.offsetTop;
-    var windowHeight = window.innerHeight;
-    var scrollY = window.scrollY;
-    var triggerOffset = 100;
-  
-    if (scrollY > boxOffsetTop - windowHeight + triggerOffset) {
-      triggerAnimation();
-      window.removeEventListener("scroll", handleScroll);
-    }
-  }
-  
-  window.addEventListener("scroll", handleScroll);
-}
-
-winner3()
-
-// winner 4
-
-function winner4 () {
-  function triggerAnimation() {
-    var box = document.getElementById("winner4");
-    box.style.opacity = '1'
-  }
-  
-  function handleScroll() {
-    var box = document.getElementById("winner4");
-    var boxOffsetTop = box.offsetTop;
-    var windowHeight = window.innerHeight;
-    var scrollY = window.scrollY;
-    var triggerOffset = 100;
-  
-    if (scrollY > boxOffsetTop - windowHeight + triggerOffset) {
-      triggerAnimation();
-      window.removeEventListener("scroll", handleScroll);
-    }
-  }
-  
-  window.addEventListener("scroll", handleScroll);
-}
-
-winner4()
-
-// winner 5
-
-function winner5 () {
-  function triggerAnimation() {
-    var box = document.getElementById("winner5");
-    box.style.opacity = '1'
-  }
-  
-  function handleScroll() {
-    var box = document.getElementById("winner5");
-    var boxOffsetTop = box.offsetTop;
-    var windowHeight = window.innerHeight;
-    var scrollY = window.scrollY;
-    var triggerOffset = 100;
-  
-    if (scrollY > boxOffsetTop - windowHeight + triggerOffset) {
-      triggerAnimation();
-      window.removeEventListener("scroll", handleScroll);
-    }
-  }
-  
-  window.addEventListener("scroll", handleScroll);
-}
-
-winner5()
-
-// winner 6 
-
-function winner6 () {
-  function triggerAnimation() {
-    var box = document.getElementById("winner6");
-    box.style.opacity = '1'
-  }
-  
-  function handleScroll() {
-    var box = document.getElementById("winner6");
-    var boxOffsetTop = box.offsetTop;
-    var windowHeight = window.innerHeight;
-    var scrollY = window.scrollY;
-    var triggerOffset = 100;
-  
-    if (scrollY > boxOffsetTop - windowHeight + triggerOffset) {
-      triggerAnimation();
-      window.removeEventListener("scroll", handleScroll);
-    }
-  }
-  
-  window.addEventListener("scroll", handleScroll);
-}
-
-winner6()
-
-// winner 7
-
-function winner7 () {
-  function triggerAnimation() {
-    var box = document.getElementById("winner7");
-    box.style.opacity = '1'
-  }
-  
-  function handleScroll() {
-    var box = document.getElementById("winner7");
-    var boxOffsetTop = box.offsetTop;
-    var windowHeight = window.innerHeight;
-    var scrollY = window.scrollY;
-    var triggerOffset = 100;
-  
-    if (scrollY > boxOffsetTop - windowHeight + triggerOffset) {
-      triggerAnimation();
-      window.removeEventListener("scroll", handleScroll);
-    }
-  }
-  
-  window.addEventListener("scroll", handleScroll);
-}
-
-winner7()
-
 function playVid(card) {
   const video = card.querySelector('.vid1');
   video.setAttribute('autoplay', 'true');
@@ -197,3 +16,39 @@ container.addEventListener('wheel', (event) => {
   event.preventDefault();
   container.scrollLeft += event.deltaY;
 });
+
+
+function openMenu() {
+  
+const header = document.getElementById('myheader')
+const nav = document.getElementById('nav')
+const ul = document.getElementById('ul')
+const body = document.getElementById('body')
+  if (header.style.maxHeight === '90px') {
+    header.style.maxHeight = '300px'
+    header.style.borderRadius = '40px'
+    header.style.animation = 'open 0.3s 1 linear'
+    ul.style.animation = 'fadeIn 1.2s 1 linear'
+    ul.style.opacity = '1'
+  } else  {
+    header.style.maxHeight = '90px'
+    header.style.animation = 'close 0.3s 1 linear'
+  }
+
+  if (header.style.maxHeight === '300px') {
+    nav.style.display = 'block'
+  }  else {
+    nav.style.display = 'none'
+  }
+}
+
+function menu() {
+  const header = document.getElementById('myheader')
+  const nav = document.getElementById('nav')
+  if (header.style.width > '925px' && body.style.width > '925px') {
+    nav.style.display = 'inline-block'
+    header.style.height = '90px'
+  } 
+}
+
+menu()
---
+++ scripts/winner.js
@@ -0,0 +1,24 @@
+// winner 1
+function winner() {
+    function triggerAnimation() {
+      var box = document.getElementById("winner");
+      box.style.opacity = '1'
+    }
+  
+    function handleScroll() {
+      var box = document.getElementById("winner");
+      var boxOffsetTop = box.offsetTop;
+      var windowHeight = window.innerHeight;
+      var scrollY = window.scrollY;
+      var triggerOffset = 100;
+  
+      if (scrollY > boxOffsetTop - windowHeight + triggerOffset) {
+        triggerAnimation();
+        window.removeEventListener("scroll", handleScroll);
+      }
+    }
+  
+    window.addEventListener("scroll", handleScroll);
+  }
+  
+  winner()
