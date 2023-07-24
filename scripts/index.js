50d85008074c3b595764bfa6d032f936990736c7
Mikhei1i <135987132+Mikhei1i@users.noreply.github.com>
2023-07-24 20:46:58 +0400

    header hamburger menu bug fix

--- scripts/index.js
+++ scripts/index.js
@@ -11,37 +11,17 @@ function pauseVid(card) {
 }
 
 
-function openMenu() {
-  
+const hamburger = document.querySelector('.fa-bars');
+const navMenu = document.getElementById('nav')
 const header = document.getElementById('myheader')
-const nav = document.getElementById('nav')
-const ul = document.getElementById('ul')
-const body = document.getElementById('body')
-  if (header.style.maxHeight === '90px') {
-    header.style.maxHeight = '300px'
-    header.style.borderRadius = '40px'
-    header.style.animation = 'open 0.3s 1 linear'
-    ul.style.animation = 'fadeIn 1.2s 1 linear'
-    ul.style.opacity = '1'
-  } else  {
-    header.style.maxHeight = '90px'
-    header.style.animation = 'close 0.3s 1 linear'
-  }
 
-  if (header.style.maxHeight === '300px') {
-    nav.style.display = 'block'
-  }  else {
-    nav.style.display = 'none'
-  }
-}
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active')
+  navMenu.classList.toggle('active')
+  header.classList.toggle('active')
+})
 
-function menu() {
-  const header = document.getElementById('myheader')
-  const nav = document.getElementById('nav')
-  if (header.style.width > '925px' && body.style.width > '925px') {
-    nav.style.display = 'inline-block'
-    header.style.height = '90px'
-  } 
-}
-
-menu()
+document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click', () => {
+  hamburger.classList.remove('active')
+  navMenu.classList.remove('active')
+}))
